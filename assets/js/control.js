141357315501f2916ab7c9e76deb6cfbeadad835
huanhuanboy <weihuan.luositing@gmail.com>
2020-03-09 13:08:32 +0800

    .

--- assets/js/control.js
+++ assets/js/control.js
@@ -18,7 +18,7 @@ if (getCookie('userid') && getCookie('userid').length >= 3) {
         document.getElementById("sid_login").addEventListener("click", function() {
             let s = document.getElementById("sid_value").value;
             var sid = { "vbracke": "1", "cnchanac": "1", "cwchanbf": "1", "ckchanbo": "1", "lchenbm": "1", "ychendm": "1", "hschengac": "1", "kkchengah": "1", "ypchengaa": "1", "tkcheungad": "1", "cwvchu": "1", "ktchung": "1", "cyfungah": "1", "psgoradia": "1", "ngaa": "1", "mhassanaa": "1", "jyho": "1", "wyhoam": "1", "fhuaa": "1", "xhuangat": "1", "skhanna": "1", "dhkimac": "1", "wykongaa": "1", "pkaa": "1", "chkwokak": "1", "alamae": "1", "alamba": "1", "cwlauag": "1", "tllauac": "1", "ksleeaf": "1", "wlleeag": "1", "ctleungad": "1", "yfleungac": "1", "hlibg": "1", "haotianli": "haotianli", "wlibs": "1", "hliubm": "1", "ylliuaa": "1", "yhload": "1", "qluag": "1", "ylubj": "1", "chmokab": "1", "hymong": "1", "nnanda": "1", "nyngaiaa": "1", "huamin": "huamin", "jdsanjuan": "1", "yshaoaj": "1", "ysshiu": "1", "wlsiuab": "1", "ttangae": "1", "kcting": "1", "hktsangab": "1", "kwtsuiab": "1", "kkwanag": "1", "twangbj": "1", "ywangct": "1", "hweiad": "1", "zwenab": "1", "clwongak": "1", "cywongbr": "1", "jywongaa": "1", "kmwongap": "1", "wkwongav": "1", "mxiaag": "1", "mxuar": "1", "chyeoh": "1", "mymyeung": "1", "nclyueh": "1", "rzhangab": "1", "yzhangfg": "1", "dzhongab": "1" }
-            if (sid[s].length > 5) {
+            if (sid[s].length) {
                 window.userID = "ipt_" + s;
                 document.getElementById("sid_container").style["display"] = "none"
                 document.getElementById("id_exercise").style["display"] = "block"
